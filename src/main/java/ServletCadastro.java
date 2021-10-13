c3a7799de457ed2f7a35c37f70ed8d7481ef1e40
Anthony_Wendy <76909963+AnthonyWendy@users.noreply.github.com>
2021-10-12 22:59:23 -0300

    Analise
    Verificando o 'import' dentro do servelet.

--- src/main/java/ServletCadastro.java
+++ src/main/java/ServletCadastro.java
@@ -1,6 +1,6 @@
 
-
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,48 +13,50 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/ServletCadastro")
 public class ServletCadastro extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletCadastro() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletCadastro() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
-		
+
 		String sName = request.getParameter("valName");
 		String iMatricula = request.getParameter("valMatricula");
 		String iNumero = request.getParameter("valnumero");
-		
+
 		Cadastra cadastra = new Cadastra();
 		cadastra.setNome(sName);
 		cadastra.setCpf(iNumero);
 		cadastra.setMatricula(iMatricula);
-		
+
 		CadastraDAO ca = new CadastraDAO();
-		
+
 		try {
 			ca.Cad_aluno(cadastra);
 		} catch (ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-		
+
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
